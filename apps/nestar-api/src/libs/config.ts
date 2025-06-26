7ec1c0b688bbf763851028ce1b995303949d5634
Rosuljon <ikramjanov97@mail.ru>
2025-06-26 12:16:21 +0900

    fix: refactoring

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,9 +1,13 @@
 import { ObjectId } from 'bson';
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
 
+//available Sorts for DTOs
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
-
 export const availableOptions = ['propertyBarter', 'propertyRent'];
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
 export const availablePropertySorts = [
 	'createdAt',
 	'updatedAt',
@@ -12,21 +16,20 @@ export const availablePropertySorts = [
 	'propertyRank',
 	'propertyPrice',
 ];
-export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
-// IMAGE CONFIGURATION (config.js)
-import { v4 as uuidv4 } from 'uuid';
-import * as path from 'path';
 
+// IMAGE CONFIGURATION
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
 	const ext = path.parse(filename).ext;
 	return uuidv4() + ext;
 };
 
+// any type => ObjectId
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
 
+//mongodb aggr.
 export const lookupMember = {
 	$lookup: {
 		from: 'members',

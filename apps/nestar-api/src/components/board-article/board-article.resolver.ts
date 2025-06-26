ab286b0437e8571cb5358902621e10ca11e40b0d
Rosuljon <ikramjanov97@mail.ru>
2025-06-26 12:01:36 +0900

    feat: develop admin boardArticle graphql api

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -3,12 +3,19 @@ import { BoardArticleService } from './board-article.service';
 import { UseGuards } from '@nestjs/common';
 import { BoardArticle, BoardArticles } from '../../libs/dto/board-article/board-article';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import { BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import {
+	AllBoardArticlesInquiry,
+	BoardArticleInput,
+	BoardArticlesInquiry,
+} from '../../libs/dto/board-article/board-article.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
 
 @Resolver()
 export class BoardArticleResolver {
@@ -58,5 +65,36 @@ export class BoardArticleResolver {
 	): Promise<BoardArticles> {
 		console.log('BoardArticleResolver.getBoardArticles called');
 		return await this.boardArticleService.getBoardArticles(memberId, input);
+	}
+
+	//ADMIN
+
+	//Get All Board Articles By Admin
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Query(() => BoardArticles)
+	public async getAllBoardArticlesByAdmin(@Args('input') input: AllBoardArticlesInquiry): Promise<BoardArticles> {
+		console.log('BoardArticleResolver.getAllBoardArticlesByAdmin called');
+		return await this.boardArticleService.getAllBoardArticlesByAdmin(input);
+	}
+
+	//Update Board Article By Admin
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => BoardArticle)
+	public async updateBoardArticleByAdmin(@Args('input') input: BoardArticleUpdate): Promise<BoardArticle> {
+		console.log('BoardArticleResolver.updateBoardArticleByAdmin called');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.boardArticleService.updateBoardArticleByAdmin(input);
+	}
+
+	//Remove Board Article By Admin
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => BoardArticle)
+	public async removeBoardArticleByAdmin(@Args('input') input: string): Promise<BoardArticle> {
+		console.log('BoardArticleResolver.removeBoardArticleByAdmin called');
+		const articleId = shapeIntoMongoObjectId(input);
+		return await this.boardArticleService.removeBoardArticleByAdmin(articleId);
 	}
 }

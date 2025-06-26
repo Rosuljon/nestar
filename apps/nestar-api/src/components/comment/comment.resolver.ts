58f168253eafa92e14d092bc4016079143fe7591
Rosuljon <ikramjanov97@mail.ru>
2025-06-26 12:56:06 +0900

    feat: develop comment related APIs

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,7 +1,51 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class CommentResolver {
 	constructor(private readonly commentService: CommentService) {}
+
+	//Create Comment
+	@UseGuards(AuthGuard)
+	@Mutation(() => Comment)
+	public async createComment(
+		@Args('input') input: CommentInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('CommentResolver.createComment called');
+		return await this.commentService.createComment(memberId, input);
+	}
+
+	//Update Comment
+	@UseGuards(AuthGuard)
+	@Mutation(() => Comment)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('CommentResolver.updateComment called');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.commentService.updateComment(memberId, input);
+	}
+
+	//Get Comments
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('CommentResolver.getComments called');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return await this.commentService.getComments(memberId, input);
+	}
 }

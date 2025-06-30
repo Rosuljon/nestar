f463445dad0ef2baf91b892a403b3fa0140b51d1
Rosuljon <ikramjanov97@mail.ru>
2025-06-30 09:50:50 +0900

    feat: develop LikeTargetMember

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -94,6 +94,18 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	//Like Target Member
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('MemberResolver.likeTargetMember called');
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTargetMember(memberId, likeRefId);
+	}
+
 	//ADMIN
 
 	// Get All Members By Admin
